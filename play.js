a48fe48c88d2c1a5bde1546fc87aa1c4624bc114
Schwartz, Alejandro <alejandromatiassch@gmail.com>
2020-09-16 00:09:12 -0300

    Mejorando el diseño de la pagina y arreglando el js para que muestre en pantalla las imagenes de resultado

--- play.js
+++ play.js
@@ -1,15 +1,38 @@
 var inicio = document.getElementById("boton");
 inicio.addEventListener("click", jugar);
 var resultado = document.getElementById("resultado");
-// console.log(jugador);
 
-var maquina = Math.floor(Math.random() * 3)+1;
 
 function jugar()
 { 
+    var maquina = Math.floor(Math.random() * 3)+1;
+    console.log(maquina);
     var jugadorcito = document.getElementById("selection");
     var jugador = parseInt(jugadorcito.value);
+    
+    // Para visualizar la mano del jugador en pantalla
+    if (jugador == 1) {
+        document.getElementById("mano1").src="images/piedra.png";
+    }
+    else if (jugador == 2) {
+        document.getElementById("mano1").src="images/papel.png";
+    }
+    else {
+        document.getElementById("mano1").src="images/tijera.png";
+    }
+    
+    // Para visualizar la mano de la maquina en pantalla
+    if (maquina == 1) {
+        document.getElementById("mano2").src="images/piedra.png";
+    }
+    else if (maquina == 2) {
+        document.getElementById("mano2").src="images/papel.png";
+    }
+    else {
+        document.getElementById("mano2").src="images/tijera.png";
+    }
 
+    // Programacion de la partida
     if (jugador == maquina) {
         resultado.innerHTML = "Empate";
     } 
